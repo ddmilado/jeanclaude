699a426b295355e4a3ebb6b09579c253580a010e
ddmilado <adebayodamilola@gmail.com>
2024-12-17 23:03:02 +0400

    new

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,10 +2,10 @@ import ogImageSrc from "@images/social.png";
 
 export const SITE = {
   title: "ARD Ghadeer Co",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
+  tagline: "Shipping and Logistics Services",
+  description: "ARD Ghadeer Co offers top-tier shipping and logistics services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+  description_short: "ARD Ghadeer Co offers top-tier shipping and logistics services to meet all your project needs.",
+  url: "https://www.ardghadeer.com",
   author: "Emil Gulamov",
 };
 
@@ -33,7 +33,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  title: `${SITE.title}: : Shipping and Logistics Services`,
+  description: "We provide top-quality shipping and logistics services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
   image: ogImageSrc,
 };
